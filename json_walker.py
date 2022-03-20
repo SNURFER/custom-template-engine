cbd18faf6805bcaaa6738d3798e3ec9fad5b55d4
SNURFER <shido322@snu.ac.kr>
2022-03-20 15:40:59 +0900

    [feat] add for loop template

--- json_walker.py
+++ json_walker.py
@@ -17,3 +17,13 @@ def find_val(user: json, key_list: []) -> str:
     return new_json[key_list[-1]]
 
 
+def find_arr(user: json, key_list: []) -> []:
+    new_json = user
+    while len(key_list) > 1:
+        key = key_list.pop(0)
+        if key in new_json:
+            new_json = new_json[key]
+        elif key.isdigit():
+            new_json = new_json[int(key)]
+
+    return new_json[key_list[-1]]
--- user_engine.py
+++ user_engine.py
@@ -16,6 +16,10 @@ class UserEngine:
         self.f = open("output.txt", "w")
         self.deque: str = ''
 
+        self.loop_flag: bool = False
+        self.loop_arr: [] = []
+        self.loop_template_str: str = ''
+
     def __del__(self):
         self.f.close()
 
@@ -25,7 +29,10 @@ class UserEngine:
                 # jump to next loop if real line break exists
                 # only user written '\n' will move to next line
                 if ch != '\n':
-                    self.write_line(ch, user)
+                    if not self.loop_flag:
+                        self.write_line(ch, user)
+                    else:
+                        self.write_loop_template(ch)
 
     def write_line(self, ch: str, user: json):
         if len(self.deque) == 0 and ch != '<':
@@ -39,16 +46,51 @@ class UserEngine:
             self.deque += ch
             # read the inline template input <?[input]?> and generate string with user data
             if ch == '>':
-                inline_str = self.deque[3:-2].strip()
-                inline_list = inline_str.split('.')
-                parsed_str = json_walker.find_val(user, inline_list[1:])
-                self.line_str += parsed_str
-                self.deque = ''
+                if self.deque[2] == '=':
+                    # access directly to data
+                    inline_str = self.deque[3:-2].strip()
+                    inline_list = inline_str.split('.')
+                    parsed_str = json_walker.find_val(user, inline_list[1:])
+                    self.line_str += parsed_str
+                    self.deque = ''
+
+                else:
+                    # for loop mode
+                    inline_str = self.deque[2:-2].strip()
+                    inline_list = inline_str.split(' ')
+                    inline_list2 = inline_list[-1].split('.')
+                    self.loop_arr = json_walker.find_val(user, inline_list2[1:])
+                    self.loop_flag = True
+                    self.loop_template_str = ''
+                    self.deque = ''
 
         # write line to file immediately when line break character detected
         if self.line_str[-2:] == '\\n':
             self.line_str = self.line_str[:-2]
             self.f.write(self.line_str + '\n')
             self.line_str = ''
+
+    def write_loop_template(self, ch: str):
+        self.loop_template_str += ch
+        if ch == '<':
+            self.deque += ch
+        elif ch == '>':
+            self.deque += ch
+            inline_str = self.deque[2:-2].strip()
+            if inline_str.find('endfor') != -1:
+                self.loop_flag = False
+                self.loop_template_str = self.loop_template_str[:-len(self.deque)]
+                # TODO write for loop template str here
+                # fixme
+                self.deque = ''
+                for item in self.loop_arr:
+                    for ch in self.loop_template_str:
+                        if ch != '\n':
+                            self.write_line(ch, item)
+
+            self.deque = ''
+        elif len(self.deque) > 0:
+            self.deque += ch
+
 
 
